8adc8dd6f26701f0fbea98e6cf91fc5dbecda2e6
bud <ben.donovan@musiclegends.net>
2025-05-08 01:38:12 +0100

    Removed home dotfiles config

--- hosts/dev-minimal/home.nix
+++ hosts/dev-minimal/home.nix
@@ -4,12 +4,6 @@
 	home.homeDirectory = "/home/bud";
 	home.stateVersion = "24.11";
 
-	home.file = {
-		".config/nvim" = {
-			source = config.lib.file.mkOutOfStoreSymlink ../../dotfiles/nvim;
-		};
-	};
-
 	programs.git = {
 		enable = true;
 		userName = "bud";
